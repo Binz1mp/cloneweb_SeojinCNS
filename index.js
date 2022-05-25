19453c074722f6e035d36020ee5a503ee75b1b24
Binz1mp <96367152+Binz1mp@users.noreply.github.com>
2022-05-25 15:10:39 +0900

    스크롤다운때 네비게이션 색상 변경

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 //////////////// 최상단 네비게이션 바 시작 ////////////////
 let header = document.querySelector("header");
 let whitebox = document.querySelector("#whitebox");
+// let whiteboxcontents = document.querySelector(".whitebox-contents");
 let mainlogo = document.getElementById("mainlogo");
 header.addEventListener("mouseover", function() {
   if (!whitebox.classList.contains("on")) {
     whitebox.classList.add('on');
-    mainlogo.setAttribute("src", "images/logo/logo_color.png")
+    // whiteboxcontents.classList.add('visible');
+    if (!header.classList.contains("downed")) {
+      mainlogo.setAttribute("src", "images/logo/logo_color.png")
+    }
   }
 });
-header.addEventListener("mouseout", function() {
-  if (whitebox.classList.contains("on")) {
+  header.addEventListener("mouseout", function() {
+    if  (whitebox.classList.contains("on")) {
     whitebox.classList.remove('on');
-    mainlogo.setAttribute("src", "images/logo/logo_white.png")
+    // whiteboxcontents.classList.remove('visible');
+    if (!header.classList.contains("downed")) {
+      mainlogo.setAttribute("src", "images/logo/logo_white.png")
+    }
   }
 });
 //////////////// 최상단 네비게이션 바 끝 ////////////////
@@ -42,5 +49,12 @@ function doScroll(scroll) {
   sections[scrolls].scrollIntoView ({
     block: "start", inline: "start", behavior: "smooth"
   });
+  if (scrolls >= 1) {
+    header.classList.add('downed');
+    mainlogo.setAttribute("src", "images/logo/logo_color.png")
+  } else if (scrolls <= 0) {
+    header.classList.remove('downed');
+    mainlogo.setAttribute("src", "images/logo/logo_white.png")
+  }
 }
 //////////////// 스크롤 업다운 섹션 이동 끝 ////////////////
